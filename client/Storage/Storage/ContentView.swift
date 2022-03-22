48de32630b5b6d77baa53c686aee823e94929f71
itsjohnye <itsjohnye@gmail.com>
2022-03-22 17:36:29 +0800

    update

--- client/Storage/Storage/ContentView.swift
+++ client/Storage/Storage/ContentView.swift
@@ -44,7 +44,7 @@ struct ContentView: View {
                     }
                 }
             }
-             .navigationTitle("Docker compose single server")
+             .navigationTitle("Docker-compose single server")
             .navigationBarTitleDisplayMode(.inline)
         }
     }
