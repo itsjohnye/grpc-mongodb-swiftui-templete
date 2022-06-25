82595ab7f26986c40d6b10c285c4ea1e2b39ca76
itsjohnye <itsjohnye@gmail.com>
2022-06-25 18:39:26 +0800

    add notes

--- client/Storage/Storage/ViewModel.swift
+++ client/Storage/Storage/ViewModel.swift
@@ -70,71 +70,73 @@ final class ViewModel: ObservableObject {
             }
         }
     }
-  
+    
 }
 
 //MARK: Nio with Combine method
 extension ViewModel {
-    private var getProfileWithNioPublisher: AnyPublisher<Storage_GetProfileResponse, GRPCStatus> {
-            Deferred{
-                Future<Storage_GetProfileResponse, GRPCStatus> { promise in
-                    let req: Storage_GetProfileRequest = .with{
-                        $0.userUuid = self.uuid
-                    }
-                    let call = self.nioClient.getProfile(req, callOptions: self.callOptions)
-                    call.response.whenSuccess{
-                        promise(.success($0))
-                    }
-                    call.status.whenSuccess{
-                        promise(.failure($0))
-                    }
+    private var getProfileWithNioPublisher: AnyPublisher<Storage_GetProfileResponse, GRPCStatus> {  //returns GRPCStatus which confirms to the Error protocol
+        Deferred{       //Using Deferred wraps Future: Wait for the subscription before executing the closure to create the Publisher for the new Subscriber.
+            Future<Storage_GetProfileResponse, GRPCStatus> { promise in //Here, a Publisher of type Future (asynchronous, one-time) is created.
+                let req: Storage_GetProfileRequest = .with{
+                    $0.userUuid = self.uuid
+                }
+                let call = self.nioClient.getProfile(req, callOptions: self.callOptions)
+                call.response.whenSuccess{
+                    promise(.success($0))   //$0 is 'syntactic sugar', indicating the first parameter, and the corresponding parameter type will be judged according to the function type. Here refers to the first parameter of the response.whenSuccess closure, which is the value of type Storage_GetProfileResponse.
+                }
+                
+                //The status EventLoofFuture succeeds when RPC fails.
+                call.status.whenSuccess{
+                    promise(.failure($0))   //$0 refers to the first parameter of the status.whenSuccess closure, which is a value of type GRPCStatus.
                 }
             }
-            .subscribe(on: DispatchQueue.global())
-            .receive(on: DispatchQueue.main)
-            .eraseToAnyPublisher()
         }
-        
-        private var updateProfileWithNioPublisher: AnyPublisher<Storage_UpdateProfileResponse, GRPCStatus> {
-            Deferred{
-                Future<Storage_UpdateProfileResponse, GRPCStatus> { promise in
-                    let req: Storage_UpdateProfileRequest = .with {
-                        $0.userUuid = self.uuid
-                        $0.name = self.name
-                        $0.habit = self.habit
-                    }
-                    let call = self.nioClient.updateProfile(req, callOptions: self.callOptions)
-                    call.response.whenSuccess{
-                        promise(.success($0))
-                    }
-                    call.status.whenSuccess{
-                        promise(.failure($0))
-                    }
+        .subscribe(on: DispatchQueue.global())  //subscribe(on:) specifies the scheduler for subscription, cancellation, and request. The above operations are performed in the child thread and do not block the main thread.
+        .receive(on: DispatchQueue.main)    //receive(on:) specifies the scheduler on which to receive elements from the publisher. From here, the Publisher dispatches the main thread, such as updating the UI.
+        .eraseToAnyPublisher()  //AnyPublisher<Scorer_UUID,Error>
+    }
+    
+    private var updateProfileWithNioPublisher: AnyPublisher<Storage_UpdateProfileResponse, GRPCStatus> {
+        Deferred{
+            Future<Storage_UpdateProfileResponse, GRPCStatus> { promise in
+                let req: Storage_UpdateProfileRequest = .with {
+                    $0.userUuid = self.uuid
+                    $0.name = self.name
+                    $0.habit = self.habit
+                }
+                let call = self.nioClient.updateProfile(req, callOptions: self.callOptions)
+                call.response.whenSuccess{
+                    promise(.success($0))
+                }
+                call.status.whenSuccess{
+                    promise(.failure($0))
                 }
             }
-            .subscribe(on: DispatchQueue.global())
-            .receive(on: DispatchQueue.main)
-            .eraseToAnyPublisher()
         }
+        .subscribe(on: DispatchQueue.global())
+        .receive(on: DispatchQueue.main)
+        .eraseToAnyPublisher()
+    }
     
     func getProfileWithNio() {
         print("getProfileWithNio function invoked")
-                getProfileWithNioPublisher
-                    .sink(receiveCompletion: {completion in
-                        switch completion {
-                        case .failure(let status):
-                            self.popupState = .gotError(status)
-                            self.isAlertPresented = true
-                        case .finished:
-                            break
-                        }
-                    }, receiveValue: { rec in
-                        print("getProfile recived.")
-                        self.name = rec.name
-                        self.habit = rec.habit
-                        
-                    })
-                    .store(in: &cancellables)
+        getProfileWithNioPublisher
+            .sink(receiveCompletion: {completion in
+                switch completion {
+                case .failure(let status):
+                    self.popupState = .gotError(status)
+                    self.isAlertPresented = true
+                case .finished:
+                    break
+                }
+            }, receiveValue: { rec in
+                print("getProfile recived.")
+                self.name = rec.name
+                self.habit = rec.habit
+                
+            })
+            .store(in: &cancellables)
     }
     
     func updateProfileWithNio()  {
